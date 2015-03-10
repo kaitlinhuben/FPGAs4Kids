3e8cc84c7ac42f6f42cd402299cbb07cad4895a3
Kaitlin Huben <kaitlin.huben@gmail.com>
2015-03-10 18:59:45 -0400

    Updated make script for new paradigm

--- make.sh
+++ make.sh
@@ -1,8 +1,2 @@
-rm -rf levels/
-elm-make src/Level_1.elm --output=Level_1.html
-elm-make src/Level_2.elm --output=Level_2.html
-elm-make src/Level_3.elm --output=Level_3.html
-elm-make src/Level_4.elm --output=Level_4.html
-elm-make src/Level_5.elm --output=Level_5.html
-mkdir levels/
-mv Level_*.html levels/
+elm-make src/Level_01.elm
+mv elm.js assets/js/Level_01.js
