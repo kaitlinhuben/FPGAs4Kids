df6c00d45c81856f4b48827270681c3766c6ee55
Kaitlin Huben <kaitlin.huben@gmail.com>
2015-04-30 10:17:19 -0400

    Some cleanup

--- src/MainRunner.elm
+++ src/MainRunner.elm
@@ -47,15 +47,55 @@ levels =
       ]
     , channels = [("inputGate", channel True)]
     } -- end Level_01
+    {-- {
+      name = "Level_02"
+    , solution = [("outputGate", True)]
+    , nextLevel = "Level_03"
+    , par = 1
+    , directions = "Testing directions"
+    , gateInfo = [
+        { name = "inputGate1"
+        , gateType = InputGate
+        , status = False
+        , inputs = []
+        , logic = "inputLogic"
+        , location = (-100,0)
+        , imgSize = (75,75)
+        }, 
+        { name = "inputGate2"
+        , gateType = InputGate
+        , status = False
+        , inputs = []
+        , logic = "inputLogic"
+        , location = (-100,-100)
+        , imgSize = (75,75)
+        }, 
+        { name = "orGate"
+        , gateType = NormalGate
+        , status = False
+        , inputs = ["inputGate1","inputGate2"]
+        , logic = "orLogic"
+        , location = (0,0)
+        , imgSize = (75,75)
+        },
+        { name = "outputGate"
+        , gateType = OutputGate
+        , status = False
+        , inputs = ["orGate"]
+        , logic = "outputLogic"
+        , location = (100,0)
+        , imgSize = (75,75)
+        }
+      ]
+    , channels = [("inputGate1", channel False), ("inputGate2", channel False)]
+    } -- end Level_02 --}
   ]
-
--- instantiate game state
--- TODO this will change between levels - keep list of levels left
 
 -- put everything into initial GameState
 gameState : GameState
 gameState = initGameState (List.head levels)
 
+-- TODO this will change between levels - keep list of levels left
 firstLevel = List.head levels
 
 -- lift input signals into user input
@@ -65,4 +105,3 @@ userInputs = fillInputsState firstLevel.channels getEmptySignalInputsState
 -- Run main
 main : Signal Element
 main = mainDriver gameState (map UserInput userInputs)
---main = Text.plainText "hello!"
